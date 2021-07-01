ff99887a2e669a3f37136d7aa2789821af268a63
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 12:19:45 -0300

    Inicialização do projeto spring boot

---
+++ src/main/java/br/com/aul2/tm/calcidade/CalcIdadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.aul2.tm.calcidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcIdadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcIdadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/aul2/tm/calcidade/CalcIdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aul2.tm.calcidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcIdadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
